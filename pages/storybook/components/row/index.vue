e579d7632de11def5c5a856b37b1b7964f8ddc5f
bocman <bocmancul@bk.ru>
2022-06-06 18:07:14 +0400

    fix

---
+++ pages/storybook/components/row/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="$style.row">
+    <div :class="$style.title">{{title}}</div>
+    <div>
+      <slot/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'StoryBookRow',
+  props: {
+    title: String
+  }
+}
+</script>
+<style lang="scss" module>
+.row {
+  //display: block;
+  padding-bottom: 2rem;
+  border-bottom: solid 1px $gray;
+  width: 100%;
+  & + .row {
+    margin-top: 3rem;
+  }
+}
+.title {
+  color: $yellow;
+  @include title-sm;
+
+}
+</style>
---
+++ pages/storybook/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div >
+    <Row title="title">
+      Фамилия Обломов не связана со словом «облом». 5 фактов о литературе из школьной программы, которых вы не знал
+    </Row>
+    <Row title="title">
+      Фамилия Обломов не связана со словом «облом». 5 фактов о литературе из школьной программы, которых вы не знал
+    </Row>
+  </div>
+</template>
+<script>
+import Row from './components/row/index'
+export default {
+  name: 'StoryBook',
+  components: {
+    Row
+  }
+}
+</script>
+<style lang="scss" module>
+.storybook {
+  display: flex;
+  flex-direction: column;
+}
+</style>
