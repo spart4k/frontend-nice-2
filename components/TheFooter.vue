b264024514c62bb27fde64be857e9aaa222374cc
Andrey <89276031447@ya.ru>
2022-06-06 15:40:38 +0400

    init project

---
+++ components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <header :class="$style.header">
+    test
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter'
+}
+</script>
+
+<style scoped lang="scss" module>
+  .header {
+    background-color: red;
+  }
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header :class="$style.header">
+    test
+  </header>
+</template>
+
+<script lang="js">
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style scoped lang="scss" module>
+.header {
+  background-color: red;
+}
+</style>
