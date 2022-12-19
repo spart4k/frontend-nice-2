3489a26a8157111905230f2f596522ce5ba1da89
Aleksey <fosterfosterfoster63@gmail.com>
2022-12-19 15:40:41 +0400

    rest console

--- store/main.js
+++ store/main.js
@@ -6,6 +6,7 @@ export const actions = {
   async getData (state, params) {
     try {
       const data = await this.$axios.post('api/v1/cardsSearch', params)
+      console.log(data.data)
       return data
     } catch (e) {
       this.$toast.error(`Ошибка: ${e.response.data.message}`, { position: 'bottom-right', icon: true })
