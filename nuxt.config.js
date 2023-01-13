c0b50c9e9cd0dfa0600a73167be441af3a1124c6
Aleksey <fosterfosterfoster63@gmail.com>
2023-01-13 18:49:59 +0400

    hide error

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     // transition: 'home',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
+      { name: 'viewport', content: 'width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no"' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
       { name: 'msapplication-TileColor', content: '#ffffff' },
