b853ad2bfd7d75d7197ceed8379701f8a57791e2
Aleksey Tihonravov <fosterfosterfoster63@gmail.com>
2023-02-02 14:48:24 +0400

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   apps: [{
-<<<<<<< HEAD
     name: 'nice-test-frontend',
-=======
-    name: 'nice-test-frontend-sandbox',
->>>>>>> 3aaa001d216f47fc36e89a2412cd0fd98d66d3fd
     script: 'npm run start'
   }]
 }
