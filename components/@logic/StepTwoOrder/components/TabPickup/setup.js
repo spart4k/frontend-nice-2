bd9fce1e67fc1d9ecb61417ca9376ed1a150c9c1
D3phyr <sanya.dadaya@mail.ru>
2023-03-24 13:48:08 +0400

    fix: delivery

--- components/@logic/StepTwoOrder/components/TabPickup/setup.js
+++ components/@logic/StepTwoOrder/components/TabPickup/setup.js
@@ -34,16 +34,14 @@ export default {
           if (!validate()) { return }
           loading.value = true
           const params = {
-            delivery_place_id: 5,
             pay_type: 1,
-            delivery_price: 0,
-            delivery_place_address: null,
-            city_id: null,
-            weight_sum: null,
-            address_id: null,
             email: formData.email,
             phone: Number(formData.phone.replace('+7', '8').replace('(', '').replace(')', '')),
-            FIO: formData.name
+            FIO: formData.name,
+            address_id: null,
+            delivery_price: 0,
+            delivery_type: 0,
+            delivery_point: null
           }
           const result = await store.dispatch('shop/createNewOrder', params)
           if (!result.data.error) {
@@ -68,16 +66,14 @@ export default {
           if (!validate()) { return }
           loading.value = true
           const params = {
-            delivery_place_id: 5,
             pay_type: activePayment.value === 'money' ? 2 : 3,
-            delivery_price: 0,
-            delivery_place_address: null,
-            city_id: null,
-            weight_sum: null,
-            address_id: null,
             email: formData.email,
             phone: Number(formData.phone.replace('+7', '8').replace('(', '').replace(')', '')),
-            FIO: formData.name
+            FIO: formData.name,
+            address_id: null,
+            delivery_price: 0,
+            delivery_type: 0,
+            delivery_point: null
           }
           const result = await store.dispatch('shop/createNewOrder', params)
           if (!result.data.error) {
